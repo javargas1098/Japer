5876dd88b36d1c7bde2855cfb4e4d5631b7cf227
javargas1098 <ja.vargas1098@gmail.com>
2022-11-16 18:15:50 -0500

    cambios finales

--- src/main/java/com/planb/jasper/controllers/VistaController.java
+++ src/main/java/com/planb/jasper/controllers/VistaController.java
@@ -3,15 +3,16 @@ package com.planb.jasper.controllers;
 import com.planb.jasper.dto.JasperDTO;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
@@ -21,7 +22,7 @@ public class VistaController {
     @GetMapping
     public String getProductList(@RequestBody List<JasperDTO> jasper, @RequestParam String reportFormat) throws FileNotFoundException, JRException {
         String path = "E:\\USER\\Projects\\PLANB\\jasper\\e-Commerce-Angular11-Springboot-PostgreSQL\\jasper";
-        File file = ResourceUtils.getFile("classpath:jasper1.jrxml");
+        File file = ResourceUtils.getFile("classpath:jasper7.jrxml");
         JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
         JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(jasper);
         Map<String, Object> parameters = new HashMap<>();
@@ -32,6 +33,41 @@ public class VistaController {
         }
         if (reportFormat.equalsIgnoreCase("pdf")) {
             JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\jasper.pdf");
+        }
+
+        return "report generated in path : " + path;
+
+    }
+
+    @GetMapping("/merged")
+    public String getProductListMerged(@RequestBody List<JasperDTO> jasper, @RequestParam String reportFormat) throws FileNotFoundException, JRException {
+        String path = "E:\\USER\\Projects\\PLANB\\jasper\\e-Commerce-Angular11-Springboot-PostgreSQL\\jasper";
+        File file = ResourceUtils.getFile("classpath:jasper2.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        List<JasperPrint> jasperPrintList = new ArrayList<>();
+        jasper.stream().forEach(data->{
+            List<JasperDTO> jasperN= new LinkedList<>();
+            jasperN.add(data);
+            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(jasperN);
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("createdBy", "Java Techie");
+            JasperPrint jasperPrint = null;
+            try {
+                jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+
+                jasperPrintList.add(jasperPrint);
+            } catch (JRException e) {
+                e.printStackTrace();
+            }
+        });
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JRPdfExporter exporter = new JRPdfExporter();
+            exporter.setExporterInput(SimpleExporterInput.getInstance(jasperPrintList));
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(path +  "/output.pdf"));
+            SimplePdfExporterConfiguration configuration = new SimplePdfExporterConfiguration();
+            configuration.setCreatingBatchModeBookmarks(true); //add this so your bookmarks work, you may set other parameters
+            exporter.setConfiguration(configuration);
+            exporter.exportReport();
         }
 
         return "report generated in path : " + path;
